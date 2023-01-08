1450f7468e659cbffc860650256e88eacbb8cd42
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2023-01-08 01:14:15 +0100

    Implemented the getCcpKey method.

--- src/utils/getCcpKey.ts
+++ src/utils/getCcpKey.ts
@@ -1,5 +1,14 @@
-// Clé CCP
+// This function is responsible for generating the CCP key, from the CCP number.
+// You can find more information about the algorithm here: https://www.youtube.com/watch?v=T7gkesAV90c
+
+const ALGO_START = 4;
 
 export default function getCcpKey(ccp: string): string {
-	return "";
+	const chars = ccp.split("").reverse();
+	const multiplied = chars.map((c, i) => Number.parseInt(c) * (i + ALGO_START));
+	const totality = multiplied.reduce((a, b) => a + b, 0);
+
+	const ccpKey = String(totality % 100).padStart(2, "0");
+
+	return ccpKey;
 }

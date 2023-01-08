adb792fcbaddcf42f2b889490ca60ed567eab63b
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2023-01-08 02:48:27 +0100

    Exported consts.

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const ALGO_START = 4; // Can be calculated, but it's always 4.
+export const emptyCcp = BigInt(99999000000000000); // Used BigInt to avoid overflow ;).
+export const algeriePosteCode = 7;
+export const MAGIC_NUM = 97; // No idea.
+export const emptyMod = 85; // emptyCcp % MAGIC_NUM. Always 85, left here for clarity.
